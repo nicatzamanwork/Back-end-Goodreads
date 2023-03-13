c40d384a4881b2d2447c0e5f5111227761a3fa3b
nicatzamanwork <webdevelopernicat@gmail.com>
2023-03-13 13:47:28 +0400

    old routes removed

--- routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { registerUser } = require("../controller/userController");
-const router = express.Router();
-
-router.post("/register", registerUser.register);
-
-module.exports = router;
